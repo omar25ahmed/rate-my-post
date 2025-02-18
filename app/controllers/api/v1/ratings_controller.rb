8a5927f209fdaa01b5f18bcfcf93b85faf652347
Omar Ragheb <ahmed.raph29@gmail.com>
2025-02-18 14:14:24 +0200

    Add ratings resources

---
+++ app/controllers/api/v1/ratings_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class RatingsController < ApplicationController
+      def create
+        rating = Posts::RatePost.call(
+          post_id: params[:post_id],
+          user_id: params[:user_id],
+          value:   params[:value]
+        )
+
+        render json: rating, status: :created
+      rescue ActiveRecord::RecordInvalid => e
+        render json: e.record.errors.full_messages, status: :unprocessable_entity
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Post or User not found' }, status: :not_found
+      end
+    end
+  end
+end
