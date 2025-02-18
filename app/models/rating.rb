98625ee32b5bc92b9856305f2bae635e8c641f0c
Omar Ragheb <ahmed.raph29@gmail.com>
2025-02-18 14:14:32 +0200

    clean up

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,9 @@
 class Rating < ApplicationRecord
+  # Associations
   belongs_to :post
   belongs_to :user
 
+  # Validations
   validates :value, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :post_id, uniqueness: { scope: :user_id, message: 'has already been rated by this user' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  # Associations
   has_many :posts
   has_many :ratings
 
+  # Validations
   validates :login, presence: true, uniqueness: true
 end
